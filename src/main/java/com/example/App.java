79c174ab26ede234b07a335c6fa7a836cf167b52
bitcse6c <bitcsec3@gmail.com>
2025-05-14 16:35:11 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,38 +4,20 @@ package com.example;
  * Hello world!
  *
  */
-import java.util.Scanner;
+
 
 public class App{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 40;
 
-        // Input 4 numbers
-        System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
+        int sum = num1 + num2 + num3 + num4;
 
-        System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
-
-        System.out.print("Enter third number: ");
-        double num3 = scanner.nextDouble();
-
-        System.out.print("Enter fourth number: ");
-        double num4 = scanner.nextDouble();
-
-        // Calculations
-        double sum = num1 + num2 + num3 + num4;
-        double average = sum / 4;
-        double product = num1 * num2 * num3 * num4;
-
-        // Output
-        System.out.println("\nResults:");
-        System.out.println("Sum: " + sum);
-        System.out.println("Average: " + average);
-        System.out.println("Product: " + product);
-
-        scanner.close();
+        System.out.println("The sum of the four numbers is: " + sum);
     }
 }
 
 
+   
